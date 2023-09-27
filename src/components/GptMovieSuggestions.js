9893f4eddb5d9baaf45531f270ec6ed839b7e862
Dhanunjaya Ginjupalli <dhanunjaya.lakshmi60@gmail.com>
2023-09-27 22:50:13 +0530

    Implemented GPT search bar component

---
+++ src/components/GptMovieSuggestions.js
@@ -0,0 +1,5 @@
+const GptMovieSuggestions = () => {
+  return <div>Movie Suggestions</div>;
+};
+
+export default GptMovieSuggestions;
--- src/components/GptSearch.js
+++ src/components/GptSearch.js
@@ -1,5 +1,17 @@
+import { LOGIN_BACKGROUND } from "../utils/constants";
+import GptMovieSuggestions from "./GptMovieSuggestions";
+import GptSearchBar from "./GptSearchBar";
+
 const GptSearch = () => {
-  return <div>GPT Search</div>;
+  return (
+    <div>
+      <div className="absolute -z-10">
+        <img src={LOGIN_BACKGROUND} alt="login-background" />
+      </div>
+      <GptSearchBar />
+      <GptMovieSuggestions />
+    </div>
+  );
 };
 
 export default GptSearch;
