a27fc9af04fa39656d4c08873623f19ec778ca85
Dhanunjaya Ginjupalli <dhanunjaya.lakshmi60@gmail.com>
2023-10-13 22:49:04 +0530

    Refactored the code to make it responsive

--- src/components/MovieInfo.js
+++ src/components/MovieInfo.js
@@ -74,30 +74,74 @@ const MovieInfo = () => {
             </button>
           </div>
         ) : (
-          <div className="mx-10 md:w-[40%] lg:w-[50%] h-[60%] absolute mt-[5%] md:mx-auto left-0 right-0 text-xl text-white bg-black bg-opacity-90 rounded-lg">
+          // <div className="mx-10 md:w-[40%] lg:w-[50%] h-[60%] absolute mt-[5%] md:mx-auto left-0 right-0 text-xl text-white bg-black bg-opacity-90 rounded-lg">
+          //   <button
+          //     className="text-3xl text-red-700 px-6 py-2 my-2"
+          //     onClick={() => navigate("/browse")}
+          //   >
+          //     Home
+          //   </button>
+          //   <div className="mx-6 flex">
+          //     <div className="w-1/3 mb-6">
+          //       <img src={IMG_CDN_URL + "w400/" + poster_path} alt="Movie" />
+          //     </div>
+
+          //     <div className="w-2/3 ml-[3%]">
+          //       <h1 className="text-3xl text-red-700 font-bold">{title}</h1>
+          //       <p className="py-2 font-bold text-slate-300 text-2xl">
+          //         Overview{" "}
+          //       </p>
+          //       <p className="py-1 w-[70%] text-xl">{overview}</p>
+          //       <p className="py-2 font-bold text-slate-300 text-2xl">
+          //         Release Date
+          //       </p>
+          //       <p className="py-1 text-xl">{release_date}</p>
+          //       <p className="py-2 font-bold text-slate-300 text-2xl">Rating</p>
+          //       <p className="py-1 text-xl">{vote_average}</p>
+
+          //       {!playVideo && (
+          //         <button
+          //           className="mx-auto font-semibold my-4 py-4 px-6 bg-red-500 text-white"
+          //           onClick={() => setPlayVideo(true)}
+          //         >
+          //           Play
+          //         </button>
+          //       )}
+          //     </div>
+          //   </div>
+          // </div>
+          <div className="p-4 lg:w-[50%] h-auto lg:h-[60%] absolute mt-4 md:mt-[5%] mx-auto left-0 right-0 text-base md:text-xl text-white bg-black bg-opacity-90 rounded-lg">
             <button
-              className="text-3xl text-red-700 px-6 py-2 my-2"
+              className="text-xl md:text-3xl text-red-700 px-4 md:px-6 py-2 my-2"
               onClick={() => navigate("/browse")}
             >
               Home
             </button>
-            <div className="mx-6 flex">
-              <div className="w-1/3 mb-6">
-                <img src={IMG_CDN_URL + "w400/" + poster_path} alt="Movie" />
+            <div className="flex flex-col md:flex-row">
+              <div className="w-full md:w-1/3 mb-6">
+                <img
+                  className="mx-auto w-[50%] sm:w-[40%] md:w-auto"
+                  src={IMG_CDN_URL + "w400/" + poster_path}
+                  alt="Movie"
+                />
               </div>
 
-              <div className="w-2/3 ml-[3%]">
-                <h1 className="text-3xl text-red-700 font-bold">{title}</h1>
-                <p className="py-2 font-bold text-slate-300 text-2xl">
-                  Overview{" "}
+              <div className="w-full md:w-2/3 ml-0 md:ml-[3%]">
+                <h1 className="text-2xl md:text-3xl text-red-700 font-bold">
+                  {title}
+                </h1>
+                <p className="py-2 font-bold text-slate-300 text-xl md:text-2xl">
+                  Overview
                 </p>
-                <p className="py-1 w-[70%] text-xl">{overview}</p>
-                <p className="py-2 font-bold text-slate-300 text-2xl">
+                <p className="py-1 text-lg md:text-xl">{overview}</p>
+                <p className="py-2 font-bold text-slate-300 text-xl md:text-2xl">
                   Release Date
                 </p>
-                <p className="py-1 text-xl">{release_date}</p>
-                <p className="py-2 font-bold text-slate-300 text-2xl">Rating</p>
-                <p className="py-1 text-xl">{vote_average}</p>
+                <p className="py-1 text-lg md:text-xl">{release_date}</p>
+                <p className="py-2 font-bold text-slate-300 text-xl md:text-2xl">
+                  Rating
+                </p>
+                <p className="py-1 text-lg md:text-xl">{vote_average}</p>
 
                 {!playVideo && (
                   <button
